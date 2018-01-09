21aeca8837d216abf70bd5b4cc259c1954558f02
AntonKuncevskiy <32430891+AntonKuncevskiy@users.noreply.github.com>
2018-01-10 00:08:07 +0200

    initial commit

---
+++ JavaScript.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+    var touch = $(".icon-menu"), menu = $(".div-memu"), iconMenu = $(".all_channels_zagolovok_b"), sidebar = $(".sidebar"), sidebarWidth = $("sidebar").width();
+
+    $(touch).on("click", function (e) {
+        e.preventDefault();
+        menu.slideToggle();
+    });
+    $(iconMenu).on("click", function (e) {
+        e.preventDefault();
+        sidebar.slideToggle();
+    });
+    //$(iconMenu).click(function (e) {
+    //    $(".header, .content").addClass("active");
+    //    if (sidebar.css("left") != sidebarWidth) {
+    //        sidebar.addClass("active");
+    //        var firstClick = true;
+    //        $(document).bind("click.myEvent", function (e) {
+    //            if (!firstClick && $(e.target).closest(".sidebar").length == 0) {
+    //                sidebar.removeClass("active");
+    //                $(".header, .content").removeClass("active");
+    //                $(document).unbind("click.myEvent");
+    //            }
+    //            firstClick = false;
+    //        })
+    //    }
+    //    e.preventDefault();
+    //});
+});
